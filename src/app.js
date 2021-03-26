d4192f26481887b142e4ec7f43ea669b444e767e
Sean Rogers <seanmikerog@gmail.com>
2021-03-26 13:12:05 -0500

    created express stuff. Added get mappings

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+
+// console.log(__dirname) // prints absolute path to this file
+console.log(__filename) // prints absolute path including this file
+
+const app = express()
+//sets publicDirectoryPath to absolute path to public folder
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+// home page is replaced with the index.html
+app.get('', (req, res) => {
+    res.send('<h1>Weather</h1>');
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(publicDirectoryPath + "/about.html");
+})
+
+app.get('/help', (req, res) => {
+    res.send({
+        name: 'Andrew',
+        age: 27
+    });
+})
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+})
